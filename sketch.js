9e926677567b07b385c757e0db1f313216e3f2ca
Aishwary200 <amritakeshri3059@gmail.com>
2020-12-22 12:38:39 +0530

    project game

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var bike, bikeImg, up;
 var backImg;
 var ground;
+var canvas
 var obstacle, obstacleImg, obstacleGroup;
 var ramp, rampImg, rampGroup
 function preload() {
@@ -11,12 +12,12 @@ function preload() {
     rampImg = loadImage("images/ramp1.png")
 }
 function setup() {
-    createCanvas(1000, 400)
+    canvas = createCanvas(displayWidth, displayHeight - 200);
     bike = createSprite(80, 300, 50, 50)
     bike.addImage(bikeImg)
     bike.scale = 0.8
     bike.debug = true
-    ground = createSprite(0, 360, 2000, 20)
+    ground = createSprite(displayWidth, displayHeight - 220, displayWidth * 2, 20)
     //ground.visible = false
     obstacleGroup = new Group();
     rampGroup = new Group();
@@ -26,10 +27,10 @@ function draw() {
     bike.collide(ground)
     ground.x = bike.x
     if (keyDown(RIGHT_ARROW)) {
-        bike.x = bike.x + 10
+        bike.x = bike.x + 20
     }
     if (keyDown(LEFT_ARROW)) {
-        bike.x = bike.x - 10
+        bike.x = bike.x - 20
     }
     if (keyDown(UP_ARROW)) {
         bike.addImage(up)
@@ -39,24 +40,28 @@ function draw() {
         bike.addImage(bikeImg)
         bike.setCollider("rectangle", 0, -10, 150, 160)
     }
-    if (bike.x % 500 === 0) {
+    if (bike.x > displayWidth) {
         camera.position.x = bike.x
     }
     //bike.x < 0 || 
-    if (keyDown("space") && bike.y > 240) {
+
+    if (keyDown("space") && bike.y > 450) {
         bike.velocityY = -10
     }
     bike.velocityY = bike.velocityY + 0.8
-    console.log(ground.x)
+    if (bike.isTouching(rampGroup)) {
+        bike.y = bike.y - 100
+        bike.x = bike.x +200
+    }
     bike.collide(obstacleGroup)
     spawnObstacle();
     spawnRamp();
     drawSprites();
-
+    console.log(bike.x)
 }
 function spawnObstacle() {
-    if (frameCount % 100 === 0) {
-        obstacle = createSprite(bike.x + 920, 310, 50, 50)
+    if (frameCount % 1000 === 0) {
+        obstacle = createSprite(bike.x + 920, displayHeight - 270, 50, 50)
         obstacle.addImage(obstacleImg)
         obstacle.scale = 0.2;
         obstacle.setCollider("circle", 0, 0, 120)
@@ -68,11 +73,10 @@ function spawnObstacle() {
 
 }
 function spawnRamp() {
-    if (frameCount % 100 === 0) {
-        ramp = createSprite(500, 310, 50, 50)
+    if (bike.x % 1000 === 0) {
+        ramp = createSprite(bike.x + 600, displayHeight - 250, 50, 50)
         ramp.addImage(rampImg)
         ramp.scale = 0.5;
-        ramp.x
         ramp.setCollider("circle", 0, 0, 120)
         ramp.collide(ground)
         ramp.debug = true
